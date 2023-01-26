e5e9a2a99b66035658d500ba21293717f952247a
Solomon ABOYEJI <solesty7@gmail.com>
2023-01-26 10:31:06 +0000

    updated repo

--- lib/src/model/role_out.g.dart
+++ lib/src/model/role_out.g.dart
@@ -72,18 +72,17 @@ class _$RoleOut extends RoleOut {
 
   @override
   int get hashCode {
-    return $jf($jc(
-        $jc(
-            $jc(
-                $jc(
-                    $jc(
-                        $jc($jc($jc(0, id.hashCode), title.hashCode),
-                            permissions.hashCode),
-                        canBeDeleted.hashCode),
-                    createdByUser.hashCode),
-                modifiedByUser.hashCode),
-            dateCreated.hashCode),
-        dateModified.hashCode));
+    var _$hash = 0;
+    _$hash = $jc(_$hash, id.hashCode);
+    _$hash = $jc(_$hash, title.hashCode);
+    _$hash = $jc(_$hash, permissions.hashCode);
+    _$hash = $jc(_$hash, canBeDeleted.hashCode);
+    _$hash = $jc(_$hash, createdByUser.hashCode);
+    _$hash = $jc(_$hash, modifiedByUser.hashCode);
+    _$hash = $jc(_$hash, dateCreated.hashCode);
+    _$hash = $jc(_$hash, dateModified.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
   }
 
   @override
@@ -214,4 +213,4 @@ class RoleOutBuilder implements Builder<RoleOut, RoleOutBuilder> {
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new,unnecessary_lambdas
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
